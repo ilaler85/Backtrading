dc004ac730fe1a8286c95de74409c17079477dec
ilaler85 <misha.galileo@yandex.ru>
2020-05-19 13:43:49 +0300

    Add files via upload

---
+++ Back/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Back
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.info = new System.Windows.Forms.ToolStripMenuItem();
+            this.run = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.riska = new System.Windows.Forms.TextBox();
+            this.riska2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.info,
+            this.run});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(400, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // info
+            // 
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(60, 24);
+            this.info.Text = "Инфо";
+            this.info.Click += new System.EventHandler(this.info_Click);
+            // 
+            // run
+            // 
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(46, 24);
+            this.run.Text = "Run";
+            this.run.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите цену деления шкалы \r\nв 1 пробирке";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "во 2 пробирке";
+            // 
+            // riska
+            // 
+            this.riska.Location = new System.Drawing.Point(34, 109);
+            this.riska.Name = "riska";
+            this.riska.Size = new System.Drawing.Size(100, 22);
+            this.riska.TabIndex = 6;
+            // 
+            // riska2
+            // 
+            this.riska2.Location = new System.Drawing.Point(272, 109);
+            this.riska2.Name = "riska2";
+            this.riska2.Size = new System.Drawing.Size(100, 22);
+            this.riska2.TabIndex = 7;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 157);
+            this.Controls.Add(this.riska2);
+            this.Controls.Add(this.riska);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Мейн";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem info;
+        private System.Windows.Forms.ToolStripMenuItem run;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox riska;
+        private System.Windows.Forms.TextBox riska2;
+    }
+}
+
